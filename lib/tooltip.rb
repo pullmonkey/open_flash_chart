898f59f413d4af2a9c509e43c170e3c6dc71c4c0
PullMonkey <charlie@pullmonkey.com>
2008-07-28 15:56:29 +0000

    get tooltips to work

---
+++ lib/tooltip.rb
@@ -0,0 +1,13 @@
+class Tooltip < OpenFlashChart
+  def set_body_style(style)
+    @body = style
+  end
+
+  def set_title_style(style)
+    @title = style
+  end
+
+  def set_background_colour(bg)
+    @background = bg
+  end
+end

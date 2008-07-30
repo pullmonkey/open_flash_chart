49497471b8d126ecff133b3d6bd4674fda59c4fd
PullMonkey <charlie@pullmonkey.com>
2008-07-30 15:38:54 +0000

    fix hbar graphs, needed the HBarValue class

--- lib/h_bar.rb
+++ lib/h_bar.rb
@@ -1,5 +1,11 @@
-class HBar < OpenFlashChart
+class HBarValue < OpenFlashChart
   def initialize(left,right)
+    @left  = left
+    @right = right
+  end
+end
+class HBar < OpenFlashChart
+  def initialize
     @type = "hbar"
     @colour = "#9933CC"
     @text = "Page Views"
--- lib/pie.rb
+++ lib/pie.rb
@@ -8,9 +8,7 @@ end
 class Pie < OpenFlashChart
   def initialize
     @type = "pie"
-    @colors = ["#d01f3c","#356aa0","#C79810"]
     @alpha = 0.6
     @border = 2
-    @values = [2,3,PieValue(6.5,"hello (6.5)")]
   end
 end

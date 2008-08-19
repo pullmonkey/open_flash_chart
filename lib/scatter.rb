4f708ee01b991c022859a3602b1f3408558ea2c5
PullMonkey <charlie@pullmonkey.com>
2008-08-19 14:43:16 +0000

    adding ScatterValue to the plugin for point based graphs

--- lib/scatter.rb
+++ lib/scatter.rb
@@ -1,3 +1,11 @@
+class ScatterValue < OpenFlashChart
+  def initialize(x,y,dot_size=-1)
+    @x = x
+    @y = y
+    @dot_size = dot_size if dot_size > 0
+  end
+end
+
 class Scatter < OpenFlashChart
   def initialize(colour, dot_size)
     @type = "scatter"

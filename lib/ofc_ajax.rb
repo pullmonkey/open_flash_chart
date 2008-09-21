70acda43eddf2715132284e2411de14e6073ecec
PullMonkey <charlie@pullmonkey.com>
2008-09-21 21:15:50 +0000

    have the user specify the div name - makes more sense to allow DOM control

--- lib/ofc_ajax.rb
+++ lib/ofc_ajax.rb
@@ -1,13 +1,5 @@
 module OFCAjax
-  def js_open_flash_chart_object(width, height, base="/")
-    special_hash = Base64.encode64(Time.now.to_f.to_s + self.render).gsub(/\n/,"")
-    obj_id   = "chart_#{special_hash}"  # some sequencing without all the work of tracking it
-    div_name = "flash_content_#{special_hash}"
-    protocol = "http" # !request.nil? ? request.env["HTTPS"] || "http" : "http"
-
-    # NOTE: users should put this in the <head> section themselves:
-    ## <script type="text/javascript" src="#{base}/javascripts/swfobject.js"></script>
-
+  def js_open_flash_chart_object(div_name, width, height, base="/")
     return %[
       <script type="text/javascript">
         swfobject.embedSWF("#{base}open-flash-chart.swf", "#{div_name}", "#{width}", "#{height}", "9.0.0");

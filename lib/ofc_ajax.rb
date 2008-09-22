619b20e8ec4ff4f073a8910524147c33eee19882
PullMonkey <charlie@pullmonkey.com>
2008-09-22 03:56:26 +0000

    added more to the javascript interface

--- lib/ofc_ajax.rb
+++ lib/ofc_ajax.rb
@@ -9,6 +9,38 @@ module OFCAjax
     ]
   end
 
+  def link_to_ofc_load(link_text, div_name)
+    data_name = "#{link_text.gsub(" ","_")}_#{div_name.gsub(" ","_")}"
+    return %[
+      <script type="text/javascript">
+        function load_#{data_name}() {
+          tmp = findSWF("#{div_name}");
+          x = tmp.load(Object.toJSON(data_#{data_name}));
+        }
+        var data_#{data_name} = #{self.render};
+      </script>
+      #{ActionView::Base.new.link_to_function link_text, "load_#{data_name}()"}
+    ]
+  end
+
+  def link_to_remote_ofc_load(link_text, div_name, url)
+    fx_name = "#{link_text.gsub(" ","_")}_#{div_name.gsub(" ","_")}"
+    #    url = CGI::escape(url)
+    return %[
+      <script type="text/javascript">
+        function reload_#{fx_name}() {
+          tmp = findSWF("#{div_name}");
+          new Ajax.Request('#{url}', {
+            method    : 'get',
+            onSuccess : function(obj) {tmp.load(obj.responseText);},
+            onFailure : function(obj) {alert("Failed to request #{url}");}});
+        }
+      </script>
+      #{ActionView::Base.new.link_to_function link_text, "reload_#{fx_name}()"}
+    ]
+  end
+
+
   def to_open_flash_chart_data
     # this builds the open_flash_chart_data js function
     return %'
